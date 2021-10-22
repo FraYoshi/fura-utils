7ac225e63feb06ee66d7182114a1467fa7fa1ab3
Francesco Yoshi Gobbo <yoshi@fgobbo.com>
2021-10-22 10:56:20 +0200

    script to set output dir

---
+++ licensing/bin/fura-reserved-batch-out.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -
+
+# sourcing artist's variables
+. ~/.config/furayoshi/licensing/artist.sh
+
+# False for public domain
+terms="True"
+license="All Rights Reserved"
+licenseLonger="$license"
+
+## arguments list ##
+# 1. file to add metadata to;
+# 2. title of the opera;
+# 3. (optional) description of the opera;
+
+# colorizations
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# read section
+if [ $1 ]; then
+    outDir="$1";
+    echo "output set to $outDir"
+else
+    echo -e "dir to output to?\n(end with a slash './')"
+    read ourDir
+fi
+
+# metadata assignment
+echo -e "writing file with exif metadata to "$outDir"\nproceed?"
+select yesno in yes no
+do case $yesno in
+       "no")
+	   echo "abort"
+	   break
+	   ;;
+       "yes")
+	   echo "starting metadata injection"
+	   find -type f -name "*" \
+	       | cut -c3- \
+	       | while read media; do
+	       exiftool -P \
+			-xmp:Marked="$terms" \
+			-xmp:UsageTerms="$licenseLonger" \
+			-xmp:Owner="$artist" \
+			-XMP-dc:Rights="$licenseLonger" \
+			-XMP-dc:Creator="$creator" \
+			-xmp:LicensorEmail="$artistEmail" \
+			-xmp:LicensorURL="$artistURL" \
+			-xmp:CopyrightOwnerID="$artistISNI" \
+			"$media" -o "$outDir""$media"
+	   done
+	   echo "end of script"
+	   break
+	   ;;
+   esac
+done
+
+# references https://wiki.creativecommons.org/wiki/Marking_Works_Technical
+# XMP tags: https://exiftool.org/TagNames/XMP.html
