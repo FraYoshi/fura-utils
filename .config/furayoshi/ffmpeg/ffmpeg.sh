4dfd8a2435969d0242430c3a8ca457af84185933
Francesco Yoshi Gobbo <yoshi@fgobbo.com>
2022-02-10 07:17:02 +0100

    desktop timelapse as image sequence
    Given the desktop coordinates, read and save desktop screenshot at a given interval.
    A config file has been provided for preferred arguments
    Thanks to As3ii for helping me with the sleep and printf part!

--- .config/furayoshi/ffmpeg/ffmpeg.sh
+++ .config/furayoshi/ffmpeg/ffmpeg.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -
 set -a
 CONCATLIST=concat-list.txt
+
+# timelapse
+TIMELAPSENAME=timelapse_
+TIMELAPSEEXT=png
+CAPTURECOORD=
 set +a
---
+++ bin/ffmpeg/fura-timelapse-desk.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -
+
+source ~/.config/furayoshi/ffmpeg/ffmpeg.sh
+
+## START of TEST CODE
+
+# to continue the progressive and not restart at 0, source the subshell
+# . fura-timelapse
+
+if [[ `ls` ]]; then
+    echo "directory ""$PWD"" not empty, input last frame +1 (without the zeros) to continue"
+    read FRAMENUM
+fi
+
+if [[ -z $DISPLAY ]]; then
+    echo  "input DISPLAY variable i.e. :0.0"
+    read DISPLAY
+fi
+
+# SECONDS is an internal variable of Bash, avoid unless want to double time at each run.
+
+if [[ $1 ]]; then
+    SECS=$1
+else
+    echo "input seconds between frames."
+    read SECS
+fi
+
+if [[ $2 ]]; then
+    CAPTURECOORD=$2
+elif
+    [[ $CAPTURECOORD ]]; then
+    CAPTURECOORD=$CAPTURECOORD
+else
+    echo -e "input desktop coordinates from xrandr. default is 0,0"
+    read CAPTURECOORD
+fi
+
+if [[ $3 ]]; then
+    TIMELAPSENAME=$3
+elif
+    [[ $TIMELAPSENAME ]]; then
+    TIMELAPSENAME=$TIMELAPSENAME
+else
+    echo "input an output name i.e. timelapse_"
+    read $TIMELAPSENAME
+fi
+
+## END of TEST CODE
+    
+while true;
+    do sleep $SECS;
+       PROGRESSIVE=$(printf "%0.5i" "$FRAMENUM");
+       ffmpeg -f x11grab -s 1920x1080 -i $DISPLAY+$CAPTURECOORD -frames:v 1 "$TIMELAPSENAME$PROGRESSIVE.$TIMELAPSEEXT";
+       FRAMENUM=$((FRAMENUM+1));
+done
