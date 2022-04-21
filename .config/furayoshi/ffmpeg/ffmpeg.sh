a9a101bd7602e5736db7377252c3abdcf2b81315
Francesco Yoshi Gobbo <yoshi@fgobbo.com>
2022-04-21 19:59:02 +0200

    to opus batch script
    Convert audio from search pattern to *.opus/*.ogg/...
    Thanks to the as3ii, Nicola Ferru and blackcodec from the Morrolinux telegram group for helping during the making of this script <3

--- .config/furayoshi/ffmpeg/ffmpeg.sh
+++ .config/furayoshi/ffmpeg/ffmpeg.sh
@@ -19,6 +19,12 @@ HYPERPRESET=placebo
 HYPERFORMAT=mp4
 HYPEROUTDIR=./
 
+# audio 2opus
+OPUSBITRATE=128k
+OPUSVBR=1 #1 on, 0 off
+OPUSCHANNELLAYOUT=stereo
+OPUSEXT=opus # ogg is more compatible, opus is suggested.
+
 # timelapse
 TIMELAPSENAME=timelapse_
 TIMELAPSEEXT=png
