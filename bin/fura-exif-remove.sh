f75dbbf4856c7bc666e2a33128f748dab6f8cd44
Francesco Yoshi Gobbo <yoshi@fgobbo.com>
2023-04-06 00:14:01 +0200

    hotfix

--- bin/fura-exif-remove.sh
+++ bin/fura-exif-remove.sh
@@ -1,21 +1,9 @@
 #!/bin/bash -
-
-# sourcing artist's variables
 source ~/.config/furayoshi/config.sh
 
-# colorizations
-RED='\033[0;31m'
-YELLOW='\033[0;33m'
-NC='\033[0m' # No Color
-
-# metadata assignment
-if [ "$1" ] && [ $2 ]; then
-    convert -resize $2 \
-	    "$1" "${1%.*}$PIC_HYPERCOMPRESS_SUFFIX.$PIC_HYPERCOMPRESS_FORMAT"
-elif
-    [ "$1" ]; then
-    convert -resize $PIC_HYPERCOMPRESS \
-	    "$1" "${1%.*}$PIC_HYPERCOMPRESS_SUFFIX.$PIC_HYPERCOMPRESS_FORMAT"
+if [ "$1" ]; then
+    exiftool -overwrite_original -All= "$1" && \
+	echo "removed metadata from $1"
 else
     printf "${RED}ERR:${NC} no input file!\n"
 fi
