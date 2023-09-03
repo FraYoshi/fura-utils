a5b2b90972a2cce073098f5f9af6e2301fd3189b
Francesco Yoshi Gobbo <yoshi@fgobbo.com>
2023-09-03 06:47:56 +0200

    directories check

--- bin/fura-compress-xz9e.sh
+++ bin/fura-compress-xz9e.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -
 
 if [ "$1" ]; then
-    xz -9e -c "$1" > "$1.xz"  \
-    && echo -en "file "$1" saved as "$1.xz" \n"
+    if [ -d "$1" ]; then
+	XZ_OPT=-e9 tar cJf "${1%/}".tar.xz "$1" \
+	    && echo -en "directory "$1" saved as "${1%/}".tar.xz \n"
+    else
+	xz -9e -c "$1" > "$1".xz  \
+	    && echo -en "file "$1" saved as "$1".xz \n"
+    fi
 else
     printf "${RED}ERR:${NC} no input file!\n"
 fi
