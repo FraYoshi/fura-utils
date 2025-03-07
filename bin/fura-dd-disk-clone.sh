25d889273f8ee2dbbfc0bd7786121b019b348c13
Francesco Yoshi Gobbo <yoshi@fgobbo.com>
2025-03-08 00:56:54 +0100

    check before execution

--- bin/fura-dd-disk-clone.sh
+++ bin/fura-dd-disk-clone.sh
@@ -3,6 +3,7 @@ source ~/.config/furayoshi/config.sh
 
 if [ -z "$1" ] || [ -z "$2" ]; then
     echo -e "${YELLOW}Usage${NC}: $scriptName <input_file> <output_file> [block_size]"
+    echo -e "${YELLOW}if using sudo${NC}: add sudo -E at the start"
     echo "Please specify the input and output files."
     exit 1
 fi
@@ -16,4 +17,25 @@ if [ "$3" ]; then
   DDBS=$3
 fi
 
-dd if=$ddif of=$ddof bs=$DDBS conv=noerror,sync,notrunc iflag=fullblock oflag=sync status=progress
+dd_command="dd if=\"$ddif\" of=\"$ddof\" bs=$DDBS conv=noerror,sync,notrunc iflag=fullblock oflag=sync status=progress"
+
+# Display the command to the user
+echo -e "${GREEN}Command to be executed:${NC}"
+echo -e "  ${dd_command}"
+
+# Prompt for confirmation
+read -p "Do you want to proceed? (yes/no) [NO]: " confirm
+
+# Set default value if no input is provided
+if [ -z "$confirm" ]; then
+  confirm="NO"
+fi
+
+# Check the user's response
+if [[ "$confirm" =~ ^[Yy][Ee][Ss]$ ]]; then
+  echo -e "${GREEN}Executing command...${NC}"
+  eval "$dd_command"
+else
+  echo -e "${RED}Execution aborted.${NC}"
+  exit 1
+fi
